2353b04f14f6ba9db3c9e4954e99112512961542
Rachit <rachitdev45@gmail.com>
2021-05-18 16:38:33 +0530

    Room screen controller is almost done

--- lib/screens/roomScreen.dart
+++ lib/screens/roomScreen.dart
@@ -27,6 +27,13 @@ class _RoomScreenState extends State<RoomScreen> {
     'transports': ['websocket'],
   });
 
+  // disconnect the Socket connection when the user leaves
+  @override
+  void dispose() {
+    socket.io.disconnect();
+    super.dispose();
+  }
+
   // keeps track of the current room state
   var currRoomState = roomState.inactive;
 
@@ -37,18 +44,27 @@ class _RoomScreenState extends State<RoomScreen> {
   // name of the players
   List listOfPlayers = ["abc", "abc", "abc", "abcd"];
 
-  //name of the host
+  // name of the host
   String host;
 
   // number of decks to be used in the game
   var numberOfDecks = 1;
+
+  // number of players
+  var numberOfPlayers = 1;
+
+  // cards per player
+  var cardsPerPlayers = 1;
+
+  // for entering number of cards per player
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController textController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     // this is done so that socket.emit() is only executed for first build
     // otherwise, with every setState() socket.emit() will be called which
     // will rebuild setting of socket.emit() again.[Looped]
-
     if (!initialized) {
       socket.emit('join', widget.username);
       initialized = true;
@@ -57,9 +73,14 @@ class _RoomScreenState extends State<RoomScreen> {
     // server sends the state of the room after receiving "join"
     // or when a user disconnected
     socket.on('join-resp', (data) {
-      // rebuild with new room state
-      print(widget.username);
+      // data is an array
+      // data[0]: state of the game,
+      // data[1]: array of the name of the players in the room
+      // data[2]: host
+      // data[3]: numberOfDecks
+      // data[4]: cards per player
       print(data);
+
       setState(() {
         if (data[0] == "active") {
           currRoomState = roomState.active;
@@ -67,17 +88,29 @@ class _RoomScreenState extends State<RoomScreen> {
         } else if (data[0] == "inactive") {
           currRoomState = roomState.inactive;
           listOfPlayers = data[1];
+          numberOfDecks = data[3];
+          cardsPerPlayers = data[4];
           host = widget.username;
+          numberOfPlayers = listOfPlayers.length;
         }
+
         // if the state is waiting and user is not the host
         else if (data[0] == "waiting" && (data[2] != widget.username)) {
           currRoomState = roomState.waiting;
           listOfPlayers = data[1];
+          numberOfDecks = data[3];
+          cardsPerPlayers = data[4];
+          numberOfPlayers = listOfPlayers.length;
         }
+
         // if the state is waiting and user is the host
         else if (data[0] == "waiting" && data[2] == widget.username) {
           currRoomState = roomState.inactive;
           listOfPlayers = data[1];
+          numberOfDecks = data[3];
+          cardsPerPlayers = data[4];
+          host = widget.username;
+          numberOfPlayers = listOfPlayers.length;
         }
       });
     });
@@ -97,7 +130,7 @@ class _RoomScreenState extends State<RoomScreen> {
     } else if (currRoomState == roomState.inactive) {
       return inactiveState(screenWidth, screenHeight);
     } else if (currRoomState == roomState.waiting) {
-      return waitingState();
+      return waitingState(screenWidth, screenHeight);
     } else
       return errorState(screenWidth);
   }
@@ -143,10 +176,37 @@ class _RoomScreenState extends State<RoomScreen> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
-            height: screenHeight / 3,
+            height: screenHeight / 2,
             width: screenWidth,
             // color: Colors.red,
             child: hostControl(),
+          ),
+          Container(
+            height: screenHeight / 2,
+            width: screenWidth,
+            // color: Colors.blue,
+            child: OnlineScroller(
+              widgetList:
+                  listOfPlayers.map((name) => CardOnline(name: name)).toList(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget waitingState(double screenWidth, double screenHeight) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.all(10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: screenHeight / 3,
+            width: screenWidth,
+            child: nonHostDisplay(),
           ),
           Container(
             height: (2 * screenHeight) / 3,
@@ -162,50 +222,153 @@ class _RoomScreenState extends State<RoomScreen> {
     );
   }
 
-  Widget waitingState() {
-    return Container(
-      color: Colors.green,
-    );
-  }
-
   // the upper part of the host screen
   // with deck control
 
   Widget hostControl() {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Container(
-                margin: EdgeInsets.all(10),
-                child: Text(
-                  "Number of decks",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                )),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: DropdownButton<int>(
-                value: numberOfDecks,
-                onChanged: (value) {
-                  setState(() {
-                    numberOfDecks = value;
-                  });
-                },
-                items:
-                    <int>[1, 2, 3, 4].map<DropdownMenuItem<int>>((int value) {
-                  return DropdownMenuItem<int>(
-                    value: value,
-                    child: Text(value.toString()),
-                  );
-                }).toList(),
+    return Center(
+      // color: Colors.amber,
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Number of decks and its drop down menu
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                  // color: Colors.red,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Number of decks",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+              Container(
+                padding: EdgeInsets.all(8),
+                // color: Colors.blue,
+                child: DropdownButton<int>(
+                  value: numberOfDecks,
+                  onChanged: (value) {
+                    setState(() {
+                      numberOfDecks = value;
+                    });
+                  },
+                  items:
+                      <int>[1, 2, 3, 4].map<DropdownMenuItem<int>>((int value) {
+                    return DropdownMenuItem<int>(
+                      value: value,
+                      child: Text(value.toString()),
+                    );
+                  }).toList(),
+                ),
               ),
-            ),
-          ]),
-        ],
+            ]),
+            // Cards per player
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    "Cards per player",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+              Container(
+                  height: 60,
+                  width: 60,
+                  // padding: EdgeInsets.all(8),
+                  child: Form(
+                      key: _formKey,
+                      child: TextFormField(
+                        controller: textController,
+                        validator: (value) {
+                          int tmp;
+                          try {
+                            tmp = int.parse(value);
+                          } catch (_) {
+                            return "invalid";
+                          }
+                          print(tmp);
+                          if (tmp < 1 ||
+                              tmp >
+                                  ((numberOfDecks * 52) / numberOfPlayers)
+                                      .floor()) {
+                            return "invalid";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ))),
+            ]),
+            Container(
+              margin: EdgeInsets.all(5),
+              child: ElevatedButton(
+                  child: Text("Set"),
+                  onPressed: () {
+                    print(";");
+                    if (_formKey.currentState.validate()) {
+                      cardsPerPlayers = int.parse(textController.text);
+                      socket.emit(
+                          'update-decks', [numberOfDecks, cardsPerPlayers]);
+                    }
+                  }),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget nonHostDisplay() {
+    return Center(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Number of decks",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+              Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    numberOfDecks.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+            ]),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    "Cards per player",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+              Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text(
+                    cardsPerPlayers.toString(),
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )),
+            ]),
+          ],
+        ),
       ),
     );
   }
